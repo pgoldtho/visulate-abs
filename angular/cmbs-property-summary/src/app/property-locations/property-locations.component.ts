b8ec95a7c526bd8c2e62f28257d1eee43d87d190
goldthorp <simon@goldthorp.com>
2018-02-01 16:00:46 -0500

    details on map page

--- angular/cmbs-property-summary/src/app/property-locations/property-locations.component.ts
+++ angular/cmbs-property-summary/src/app/property-locations/property-locations.component.ts
@@ -4,6 +4,10 @@ import { Location } from '@angular/common';
 
 import { UsIndexService } from '../us-index.service';
 import { UsSummary } from '../us-summary';
+import { Observable } from "rxjs/Observable";
+import { CollectionViewer } from "@angular/cdk/collections";
+import { DataSource } from "@angular/cdk/collections";
+
 
 @Component({
   selector: 'app-property-locations',
@@ -12,14 +16,25 @@ import { UsSummary } from '../us-summary';
 })
 export class PropertyLocationsComponent implements OnInit {
   stateSummary: UsSummary[];
+  assetDetail: UsSummary[];
 
   title: string = 'My first AGM project';
   lat: number = 51.678418;
   lng: number = 7.809007;
 
+  header: string;
+
+  propShowing = false;
+  assetShowing = false;
+
+  property = [];
+  asset = [];
+
+  map: any;
+
   private convertStringToNumber(value: string): number {
-        return +value;
-    }
+    return +value;
+  }
 
   constructor(
     private route: ActivatedRoute,
@@ -45,4 +60,67 @@ export class PropertyLocationsComponent implements OnInit {
     });
   }
 
+  showDetails(state, type, name) {
+    this.indexService.getAssetDetails(state, type, name).subscribe( UsSummary => {
+      this.assetDetail = UsSummary;
+      let property = this.assetDetail[0]['property'];
+      this.header = property.propertyName;
+      this.property = Object.keys(property).map(k => ({ name: k, value: property[k] }));
+      this.property = this.property.filter(i => i.name !== 'location');
+      this.propDataSource = new MyDataSource(this.property);
+      let asset = this.assetDetail[0]['asset'];
+      this.asset = Object.keys(asset).map(k => ({ name: k, value: asset[k]}));
+      this.assetDataSource = new MyDataSource(this.asset);
+    });
+  }
+
+  displayedColumns = ['name', 'value'];
+  propDataSource: MyDataSource;
+  assetDataSource: MyDataSource;
+
+  spaceAndCap(name: string) {
+    let result = name.replace(/[A-Z]/g, c => ' '+c);
+    return result.replace(result.substring(0, 1), result.substring(0, 1).toUpperCase());
+  }
+
+  windowClosed() {
+    this.assetDetail = null;
+  }
+
+  // mapReady(map) {
+  //   this.map = map;
+  //   this.map.fitBounds(this.getMapBounds());
+  // }
+
+  // getMapBounds() {
+  //   if(this.stateSummary) {
+  //     let bounds: LatLngBounds = new google.maps.LatLngBounds();
+  //     for(let m of this.stateSummary['property']) {
+  //       if(m.location) {
+  //         bounds.extend(new google.maps.LatLng(this.convertStringToNumber(m.location.lat), this.convertStringToNumber(m.location.lon)))
+  //       }
+  //     }
+  //     return bounds;
+  //   }
+  // }
+
+}
+
+export class MyDataSource extends DataSource<Element> {
+
+  constructor(private element: Element[]) {
+    super();
+  }
+
+  connect(): Observable<Element[]> {
+    return Observable.of(this.element);
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void { }
+
+}
+
+export interface Element {
+  name: string;
+  value: string;
 }
