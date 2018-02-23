7ac6add674f79a3ebdd6f371f812a72dba1bca80
Peter Goldthorp <peter@goldthorp.com>
2018-02-22 20:04:37 -0500

    Constant for API location

--- angular/cmbs-property-summary/src/app/constants.ts
+++ angular/cmbs-property-summary/src/app/constants.ts
@@ -1,1 +1,2 @@
 export const GOOGLE_MAPS_APIKEY: string = 'AIzaSyBMD21JpmlHER9Cdvi3mrA0vw8yQ6U5xS4';
+export const API_BASE_URL: string = 'https://api.visulate.net/';
--- angular/cmbs-property-summary/src/app/us-index.service.ts
+++ angular/cmbs-property-summary/src/app/us-index.service.ts
@@ -3,6 +3,7 @@ import {  HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import { UsSummary} from './us-summary';
+import { API_BASE_URL } from './constants';
 
 @Injectable()
 export class UsIndexService {
@@ -13,15 +14,15 @@ export class UsIndexService {
     private http: HttpClient ) { }
 
     getUsSummary(): Observable<UsSummary[]> {
-      return this.http.get<UsSummary[]>('http://localhost');
+      return this.http.get<UsSummary[]>(API_BASE_URL);
     }
 
     getStateSummary(state, useCode): Observable<UsSummary[]> {
-      return this.http.get<UsSummary[]>('http://localhost/type/'+state+'/'+useCode);
+      return this.http.get<UsSummary[]>(API_BASE_URL + 'type/'+state+'/'+useCode);
     }
 
     getAssetDetails(state, useCode, name): Observable<UsSummary[]> {
-      return this.http.get<UsSummary[]>('http://localhost/asset/'+state+'/'+useCode+'/'+name);
+      return this.http.get<UsSummary[]>(API_BASE_URL + 'asset/'+state+'/'+useCode+'/'+name);
     }
 
 }
