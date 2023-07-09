8ad5426877066bba71e7d52ae6ecce0ad305533c
pgoldtho <peter@goldthorp.com>
2023-07-09 17:53:24 -0400

    Download EX-102 file data and store in a postgres table

--- cmbs/app.js
+++ cmbs/app.js
@@ -19,11 +19,46 @@ const express    = require('express');
 const fileUtils  = require('./utils/file-utils.js');
 const objectUtils = require('./utils/object-utils.js');
 const config = require('./resources/http-config.js');
+const database = require('./services/database.js');
+const http = require('./services/http.js');
 
 const app = express();
 const port = config.port;
 
+/**
+ * POST /cmbs/:form
+ *
+ * Update the database with filing data of type ABS-EE or FWP
+ * ABS-EE submissions populate the exh_102_exhibits table
+ * with data from the exhibit 102 files
+ *
+ * FWP submissions (will) populate the fwp table with Free Writing Prospectus submissions
+ *
+ */
 
+app.post('/cmbs/:form', async (req, res) => {
+
+  const form = req.params.form;
+  // validate form is a valid form type
+  if (!['ABS-EE', 'FWP'].includes(form)) {
+    res.status(400).send(`Invalid form type: ${form}`);
+  }
+  const files = fileUtils.grepFiles(config.absDirectory, form);
+  const eList = objectUtils.filterAutoIssuers(files, form);
+
+  const response = form === 'FWP' ? await http.insertAllFwpData(eList) : await http.insertAllExhibitData(eList);
+  res.json(response);
+
+});
+
+/**
+ * GET /cmbs/:form
+ *
+ * Get an array of objects containing submission data for all entities that have
+ * submitted filings of type ABS-EE. Passing a form type of FWP will return an array
+ * of FWP submissions for those entities.
+ *
+ */
 
 app.get('/cmbs/:form', (req, res) => {
   const form = req.params.form;
@@ -32,24 +67,7 @@ app.get('/cmbs/:form', (req, res) => {
     res.status(400).send(`Invalid form type: ${form}`);
   }
   const files = fileUtils.grepFiles(config.absDirectory, form);
-  const eList = [];
-  files.forEach(file => {
-    const e = fileUtils.parseJson(file);
-    // Filter out Auto loan securitizations by checking for words like 'Auto', 'BMW', 'Lease', etc.
-    if (e.name && (!(e.name.match(/auto/i) || e.name.match(/carvana/i) ||
-                     e.name.match(/bmw/i) || e.name.match(/mercedes/i) || e.name.match(/honda/i) ||e.name.match(/hyundai/i) ||
-                     e.name.match(/lease/i) ||e.name.match(/leasing/i) || e.name.match(/funding/i) ||
-                     e.name.match(/motorcycle/i) || e.name.match(/harley-davidson/i)
-                     ))) {
-
-      eList.push({
-        cik: e.cik,
-        name: e.name,
-        url: `https://www.sec.gov/Archives/edgar/data/${e.cik}`,
-        filings: objectUtils.extractFilingsByFormType(e, form)
-      });
-    }
-  });
+  const eList = objectUtils.filterAutoIssuers(files, form);
   if (req.headers.accept.includes('application/json')) {
     res.json(eList);
   } else {
@@ -57,5 +75,38 @@ app.get('/cmbs/:form', (req, res) => {
   }
 });
 
+
+/**
+ * GET /filing/:cik/:accession_number
+ *
+ * Return the json representation stored in the database
+ * of the exhibit 102 file for the specified CIK and accession number
+ *
+ */
+
+app.get('/filing/:cik/:accession_number', async (req, res) => {
+  const cik = req.params.cik;
+  const accessionNumber = req.params.accession_number;
+  const response = await database.getExhibit(cik, accessionNumber);
+  res.json(response);
+});
+
+
+/**
+ *
+ * GET /cik/:cik
+ *
+ * Get the json representation for the index.xml for the
+ * first ABS-EE filing for the specified CIK. (used for debugging)
+ */
+
+app.get('/cik/:cik', async (req, res) => {
+  const cik = req.params.cik;
+  const fileObject = fileUtils.parseJson(`${config.absDirectory}/CIK000${cik}.json`);
+  const filings = objectUtils.extractFilingsByFormType(fileObject, 'ABS-EE');
+  const response = await http.getExhibitData(`${filings[0].url}/index.xml`);
+
+  res.json(response);
+});
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)});
--- cmbs/services/database.js
+++ cmbs/services/database.js
@@ -1,19 +1,103 @@
-// Desc: Database connection and queries
-// Commented out for future use
+/*!
+ * Copyright 2023 Visulate LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// const pgp = require('pg-promise')(/* options */)
-// const db = pgp('postgres://pgoldtho:save.cmbs.json@localhost:5432/cmbs')
+const pgp = require('pg-promise')();
+const config = require('../resources/http-config');
 
-// app.get('/db', async (req, res) => {
+const db = pgp(config.postgresConfig);
 
-//   db.one('SELECT $1 AS value', 'Hello from Postgres')
-//   .then((data) => {
-//     // console.log('DATA:', data.value)
-//     res.send(data.value);
-//   })
-//   .catch((error) => {
-//     console.log('ERROR:', error)
-//   })
+/**
+ * existingExhibits
+ *
+ * Return a list accessionNumbers in the database for a given CIK
+ *
+ * @param {string} cik - CIK to search for
+ * @returns {array} - Array of CIKs
+ *
+ */
+
+async function existingExhibits(cik) {
+  const exhibits = await db.any('SELECT accession_number FROM exh_102_exhibits WHERE cik = $1', cik);
+  const accessionNumbers = exhibits.map(exhibit => exhibit.accession_number);
+  return accessionNumbers;
+}
+
+module.exports.existingExhibits = existingExhibits;
 
 
-// });
+/**
+ * saveExhibit
+ *
+ * @param {object} filing
+ * @param {object} exhibit
+ * @returns string
+ *
+ */
+
+async function saveExhibit(filing, exhibit) {
+  try {
+    // Write object to Postgres table
+    const query =
+      'INSERT INTO exh_102_exhibits ' +
+      '(cik, accession_number, filing_date, report_date, ' +
+      ' primary_document, form, size, url, exhibit_data) ' +
+      ' VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+    const values = [
+      filing.cik,
+      filing.accessionNumber,
+      filing.filingDate,
+      filing.reportDate,
+      filing.primaryDocument,
+      filing.form,
+      filing.size,
+      filing.url,
+      JSON.stringify(exhibit)
+    ];
+
+    await db.none(query, values);
+
+    return 'Success!';
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+    return `Error: ${error.message}`;
+  }
+}
+module.exports.saveExhibit = saveExhibit;
+
+/**
+ * getExhibit
+ *
+ * Return exhibit data for a given CIK and accession number
+ *
+ * @param {string} cik - CIK to search for
+ * @param {string} accessionNumber - Accession number to search for
+ *
+ * @returns {JSON} - Exhibit data
+ *
+ */
+
+async function getExhibit(cik, accessionNumber) {
+  try {
+    const query = 'SELECT exhibit_data FROM exh_102_exhibits WHERE cik = $1 AND accession_number = $2';
+    const values = [cik, accessionNumber];
+
+    const data = await db.any(query, values);
+    return data;
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+  }
+}
+module.exports.getExhibit = getExhibit;
